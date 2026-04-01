5974fba2824c09637b99267272b943284da230b0
Sheik Mohammed Ali M <69857002+sheik-md-ali@users.noreply.github.com>
2026-04-01 13:23:59 +0530

    Add Reindex AI Search button in admin products page

--- client/app/containers/Product/List.js
+++ client/app/containers/Product/List.js
@@ -7,21 +7,52 @@
 import React from 'react';
 
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 import actions from '../../actions';
+import { API_URL, ROLES } from '../../constants';
 
 import ProductList from '../../components/Manager/ProductList';
 import SubPage from '../../components/Manager/SubPage';
 import LoadingIndicator from '../../components/Common/LoadingIndicator';
 import NotFound from '../../components/Common/NotFound';
+import Button from '../../components/Common/Button';
 
 class List extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reindexing: false,
+      reindexMessage: ''
+    };
+  }
+
   componentDidMount() {
     this.props.fetchProducts();
   }
 
+  handleReindex() {
+    this.setState({ reindexing: true, reindexMessage: '' });
+
+    axios
+      .post(API_URL + '/product/reindex-all')
+      .then(function (response) {
+        this.setState({
+          reindexing: false,
+          reindexMessage: response.data.message
+        });
+      }.bind(this))
+      .catch(function () {
+        this.setState({
+          reindexing: false,
+          reindexMessage: 'Reindex failed. Try again.'
+        });
+      }.bind(this));
+  }
+
   render() {
-    const { history, products, isLoading } = this.props;
+    const { history, products, isLoading, user } = this.props;
+    const { reindexing, reindexMessage } = this.state;
 
     return (
       <>
@@ -30,6 +61,18 @@ class List extends React.PureComponent {
           actionTitle='Add'
           handleAction={() => history.push('/dashboard/product/add')}
         >
+          {user && user.role === ROLES.Admin && (
+            <div className='mb-3 d-flex align-items-center'>
+              <Button
+                text={reindexing ? 'Reindexing AI Search...' : 'Reindex AI Search'}
+                variant='primary'
+                disabled={reindexing}
+                onClick={() => this.handleReindex()}
+              />
+              {reindexing && <i className='fa fa-spinner fa-spin ml-2' />}
+              {reindexMessage && <span className='ml-2 text-success'>{reindexMessage}</span>}
+            </div>
+          )}
           {isLoading ? (
             <LoadingIndicator inline />
           ) : products.length > 0 ? (
